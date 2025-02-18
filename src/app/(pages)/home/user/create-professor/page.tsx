caad13650ebc20505e0d5b4df38d001a0e99f93e
SaldanhaElisson <elissonsaldanha19@gmail.com>
2025-02-18 09:08:48 -0300

    remove trash

--- src/app/(pages)/home/user/create-professor/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function CreateProfessor() {
-  return <>{"Criar Professor"}</>;
-}
--- src/app/(pages)/home/user/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function User({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return <>{children}</>;
-}
